7becdbe0fd8cebd97c895140dc1fda8be29b45de
heyunan111 <2742706331@qq.com>
2023-02-19 18:27:58 +0800

    Initial commit

--- stl/memory.h
+++ stl/memory.h
@@ -51,6 +51,78 @@ namespace hyn {
 
         /*************************************************************************************************************************/
         //temporary_buffer 缓冲区的申请与释放
+        template<class T, class ForwardIterator>
+        class temporary_buffer {
+        private:
+            ptrdiff_t original_len{}; //缓冲区申请大小
+            ptrdiff_t len{};          //缓冲区实际大小
+            T *buffer;              //指向缓冲区的指针
+        public:
+            temporary_buffer(ForwardIterator first, ForwardIterator last);
 
+            ~temporary_buffer() {
+                hyn::stl::destroy(buffer, buffer + len);
+                free(buffer);
+            }
+
+            ptrdiff_t size() const noexcept {
+                return len;
+            }
+
+            ptrdiff_t requested_size() const noexcept {
+                return original_len;
+            }
+
+            T *begin() noexcept {
+                return buffer;
+            }
+
+            T *end() noexcept {
+                return buffer + len;
+            }
+
+        private:
+            void allocate_buff();
+
+            void initialize_buffer(const T &, std::true_type) {}
+
+            void initialize_buffer(const T &value, std::false_type) {
+                hyn::stl::uninitialized_fill_n(buffer, len, value);
+            }
+
+        public:
+
+            temporary_buffer(const temporary_buffer &) = delete;
+
+            void operator=(const temporary_buffer &) = delete;
+        };
+
+        template<class T, class ForwardIterator>
+        void temporary_buffer<T, ForwardIterator>::allocate_buff() {
+            original_len = len;
+            if (len > static_cast<ptrdiff_t>(std::numeric_limits<int>::max() / sizeof(T)))
+                len = std::numeric_limits<int>::max() / sizeof(T);
+            while (len > 0) {
+                buffer = static_cast<T *>(malloc(len * sizeof(T)));
+                if (buffer)
+                    break;
+                len /= 2;
+            }
+        }
+
+        template<class T, class ForwardIterator>
+        temporary_buffer<T, ForwardIterator>::temporary_buffer(ForwardIterator first, ForwardIterator last) {
+            try {
+                len = hyn::stl::distance(first, last);
+                allocate_buff();
+                if (len > 0) {
+                    initialize_buffer(*first, std::is_trivially_default_constructible<T>());
+                }
+            } catch (...) {
+                free(buffer);
+                buffer = nullptr;
+                len = 0;
+            }
+        }
     }//namespace
 }//namespace
