d8dccac7bf2e0560e8b0a2f0706a2e1077c6ab44
heyunan111 <2742706331@qq.com>
2023-02-25 15:03:10 +0800

    Initial commit

--- stl/functional.h
+++ stl/functional.h
@@ -81,6 +81,84 @@ namespace hyn {
             }
         };
 
+        template<class T>
+        struct greater : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) {
+                return x > y;
+            }
+        };
+
+        template<class T>
+        struct less : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) {
+                return x < y;
+            }
+        };
+
+        template<class T>
+        struct greater_equal : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) {
+                return x >= y;
+            }
+        };
+
+        template<class T>
+        struct less_equal : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) {
+                return x <= y;
+            }
+        };
+
+
+        template<class T>
+        struct logical_and : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) const { return x && y; }
+        };
+
+
+        template<class T>
+        struct logical_or : public binary_function<T, T, bool> {
+            bool operator()(const T &x, const T &y) const { return x || y; }
+        };
+
+
+        template<class T>
+        struct logical_not : public unarg_function<T, bool> {
+            bool operator()(const T &x) const { return !x; }
+        };
+
+
+        template<class T>
+        struct identity : public unarg_function<T, bool> {
+            const T &operator()(const T &x) const { return x; }
+        };
+
+
+        template<class Pair>
+        struct select_first : public unarg_function<Pair, typename Pair::first_type> {
+            const typename Pair::first_type &operator()(const Pair &x) const {
+                return x.first;
+            }
+        };
+
+        template<class Pair>
+        struct select_second : public unarg_function<Pair, typename Pair::second_type> {
+            const typename Pair::second_type &operator()(const Pair &x) const {
+                return x.second;
+            }
+        };
+
+        template<class Arg1, class Arg2>
+        struct project_first : public binary_function<Arg1, Arg2, Arg1> {
+            Arg1 operator()(const Arg1 &x, const Arg2 &) const { return x; }
+        };
+
+        template<class Arg1, class Arg2>
+        struct project_second : public binary_function<Arg1, Arg2, Arg1> {
+            Arg2 operator()(const Arg1 &, const Arg2 &y) const { return y; }
+        };
+
+        /**************************************************************************************************************************/
 
     }//namespace
 }//namespace
