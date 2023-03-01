9d03b86c9c5c4e169a46e51e31654b3890825712
heyunan111 <2742706331@qq.com>
2023-03-01 21:38:54 +0800

    Initial commit

--- stl/set_algo.h
+++ stl/set_algo.h
@@ -9,6 +9,88 @@
 
 namespace hyn {
     namespace stl {
+        //set_union
+        template<class InputIter1, class InputIter2, class OutputIter>
+        OutputIter
+        set_union(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result) {
+            while (first1 != last1 && first2 != last2) {
+                if (*first1 < *first2) {
+                    *result = *first1;
+                    ++first1;
+                } else if (*first2 < *first1) {
+                    *result = *first2;
+                    ++first2;
+                } else {
+                    *result = *first1;
+                    ++first1;
+                    ++first2;
+                }
+                ++result;
+            }
+            return hyn::stl::copy(first2, last2, copy(first1, first2, result));
+        }
 
+        template<class InputIter1, class InputIter2, class OutputIter, class Compared>
+        OutputIter
+        set_union(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result,
+                  Compared comp) {
+            while (first1 != last1 && first2 != last2) {
+                if (comp(*first1, *first2)) {
+                    *result = *first1;
+                    ++first1;
+                } else if (comp(*first2, *first1)) {
+                    *result = *first2;
+                    ++first2;
+                } else {
+                    *result = *first1;
+                    ++first1;
+                    ++first2;
+                }
+                ++result;
+            }
+            return hyn::stl::copy(first2, last2, hyn::stl::copy(first1, last1, result));
+        }
+
+        /**********************************************************************************************************/
+        //set_intersection
+        template<class InputIter1, class InputIter2, class OutputIter>
+        OutputIter
+        set_intersection(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result) {
+            while (first1 != last1 && first2 != last2) {
+                if (*first1 < *first2) {
+                    ++first1;
+                } else if (*first2 < *first1) {
+                    ++first2;
+                } else {
+                    *result = *first1;
+                    ++first1;
+                    ++first2;
+                    ++result;
+                }
+            }
+            return result;
+        }
+
+        template<class InputIter1, class InputIter2, class OutputIter, class Compared>
+        OutputIter
+        set_intersection(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result,
+                         Compared comp) {
+            while (first1 != last1 && first2 != last2) {
+                if (comp(*first1, *first2)) {
+                    ++first1;
+                } else if (comp(*first2, *first1)) {
+                    ++first2;
+                } else {
+                    *result = *first1;
+                    ++first1;
+                    ++first2;
+                    ++result;
+                }
+            }
+            return result;
+        }
+
+        /**********************************************************************************************************/
+        //set_difference
     }//namespace
 }//namespace
