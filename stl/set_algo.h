95693ac721948a95de2337018c39688d6eaca389
heyunan111 <2742706331@qq.com>
2023-03-02 17:34:21 +0800

    Initial commit

--- stl/set_algo.h
+++ stl/set_algo.h
@@ -92,5 +92,85 @@ namespace hyn {
 
         /**********************************************************************************************************/
         //set_difference
+        template<class InputIter1, class InputIter2, class OutputIter>
+        OutputIter
+        set_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result) {
+            while (first1 != last1 && first2 != last2) {
+                if (*first1 < *first2) {
+                    *result = *first1;
+                    ++first1;
+                    ++result;
+                } else if (*first2 < *first1) {
+                    ++first2;
+                } else {
+                    ++first1;
+                    ++first2;
+                }
+            }
+            return hyn::stl::copy(first1, last1, result);
+        }
+
+        template<class InputIter1, class InputIter2, class OutputIter, class Compared>
+        OutputIter
+        set_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, OutputIter result,
+                       Compared comp) {
+            while (first1 != last1 && first2 != last2) {
+                if (comp(*first1, *first2)) {
+                    *result = *first1;
+                    ++first1;
+                    ++result;
+                } else if (comp(*first2, *first1)) {
+                    ++first2;
+                } else {
+                    ++first1;
+                    ++first2;
+                }
+            }
+            return hyn::stl::copy(first1, last1, result);
+        }
+
+        /*****************************************************************************************/
+        // set_symmetric_difference
+
+        template<class InputIter1, class InputIter2, class OutputIter>
+        OutputIter set_symmetric_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2,
+                                            OutputIter result) {
+            while (first1 != last1 && first2 != last2) {
+                if (*first1 < *first2) {
+                    *result = *first1;
+                    ++first1;
+                    ++result;
+                } else if (*first2 < *first1) {
+                    *result = *first2;
+                    ++first2;
+                    ++result;
+                } else {
+                    ++first1;
+                    ++first2;
+                }
+            }
+            return hyn::stl::copy(first2, last2, hyn::stl::copy(first1, last1, result));
+        }
+
+        template<class InputIter1, class InputIter2, class OutputIter, class Compared>
+        OutputIter set_symmetric_difference(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2,
+                                            OutputIter result, Compared comp) {
+            while (first1 != last1 && first2 != last2) {
+                if (comp(*first1, *first2)) {
+                    *result = *first1;
+                    ++first1;
+                    ++result;
+                } else if (comp(*first2, *first1)) {
+                    *result = *first2;
+                    ++first2;
+                    ++result;
+                } else {
+                    ++first1;
+                    ++first2;
+                }
+            }
+            return hyn::stl::copy(first2, last2, hyn::stl::copy(first1, last1, result));
+        }
+
     }//namespace
 }//namespace
